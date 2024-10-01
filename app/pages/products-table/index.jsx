1a97733b03243c08817a0353172fcd017f5bfe7b
milashkina <milakina.alena.developer@gmail.com>
2024-10-01 13:07:15 +0300

    add

--- app/pages/products-table/index.jsx
+++ app/pages/products-table/index.jsx
@@ -14,9 +14,8 @@ import {
 
 export const ProductsTable = () => {
     const [file, setFile] = useState([[]])
-    let totalQtys  = 0
-    let totalValues = 0
-    let averagePrice = 0
+    const [totalQtys, setTotalQtys]  = useState(0)
+    const [totalValues, setTotalValues]  = useState(0)
 
     useEffect(() => {
         totals()
@@ -25,14 +24,14 @@ export const ProductsTable = () => {
     const totals = () => (
       Array.isArray(file) && file.slice(2,file.length).map((el) => (
         el.map((e, index) => (
-            index === 4 ? totalValues = Number(totalValues) + Number(e) : null,
-            index === 5 ? totalQtys = Number(totalQtys) + Number(e) : null,
-            averagePrice = Number(totalQtys / totalValues).toFixed(2)
+           index === 5 ? setTotalValues(prevState => Number(prevState) + Number(e)) : null,
+           index === 4 ? setTotalQtys(prevState => Number(prevState) + Number(e)) : null
         ))
       ))
     )
     const uploadFileHandler = () => {
-        console.log(event.target.files?.[0])
+        setTotalQtys(0)
+        setTotalValues(0)
         if (event.target.files?.[0] && event.target.files?.[0].type === 'application/vnd.ms-excel') {
             Papa.parse(event.target.files?.[0], {
                 complete: function(results) {
@@ -54,59 +53,57 @@ export const ProductsTable = () => {
                accept={acceptFileTypes}
                onChange={uploadFileHandler}
                className={'m-2'}/>
-           <div>
-               <div className={'flex flex-row items-stretch gap-4 m-3'}>
-                   <Card x-chunk="dashboard-01-chunk-0">
-                       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                           <CardTitle className="text-sm font-medium">
-                               Total value
-                           </CardTitle>
-                       </CardHeader>
-                       <CardContent>
-                           <div className="text-2xl font-bold">{totalValues}</div>
-                       </CardContent>
-                   </Card>
-                   <Card x-chunk="dashboard-01-chunk-0">
-                       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                           <CardTitle className="text-sm font-medium">
-                               Average price
-                           </CardTitle>
-                       </CardHeader>
-                       <CardContent>
-                           <div className="text-2xl font-bold">{averagePrice}</div>
-                       </CardContent>
-                   </Card>
-                   <Card x-chunk="dashboard-01-chunk-0">
-                       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                           <CardTitle className="text-sm font-medium">
-                               Total Qty
-                           </CardTitle>
-                       </CardHeader>
-                       <CardContent>
-                           <div className="text-2xl font-bold">{totalQtys}</div>
-                       </CardContent>
-                   </Card>
-               </div>
-               <Table>
-                   <TableCaption>A list of products</TableCaption>
-                   <TableHeader>
-                       <TableRow>
-                           {(Array.isArray(file[0])) && file[0].map((el, index) => (
-                               <TableHead className="w-[100px]" key={index}>{el}</TableHead>
-                           ))}
-                       </TableRow>
-                   </TableHeader>
-                   <TableBody>
-                       {Array.isArray(file) && file.slice(1,file.length).map((el, index) => (
-                            <TableRow key={index}>
-                               {el.map((e, index) => (
-                                   <TableCell key={index}>{e}</TableCell>
-                                ))}
-                            </TableRow>
-                       ))}
-                   </TableBody>
-               </Table>
+           <div className={'flex flex-row justify-evenly gap-4 m-3'}>
+               <Card x-chunk="dashboard-01-chunk-0">
+                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                       <CardTitle className="text-sm font-medium">
+                           Total value
+                       </CardTitle>
+                   </CardHeader>
+                   <CardContent>
+                       <div className="text-2xl font-bold">{totalValues}</div>
+                   </CardContent>
+               </Card>
+               <Card x-chunk="dashboard-01-chunk-0">
+                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                       <CardTitle className="text-sm font-medium">
+                           Total Qty
+                       </CardTitle>
+                   </CardHeader>
+                   <CardContent>
+                       <div className="text-2xl font-bold">{totalQtys}</div>
+                   </CardContent>
+               </Card>
+               <Card x-chunk="dashboard-01-chunk-0">
+                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                       <CardTitle className="text-sm font-medium">
+                           Average price
+                       </CardTitle>
+                   </CardHeader>
+                   <CardContent className="text-2xl font-bold">
+                       {totalQtys && totalValues && <div>{(totalValues / totalQtys).toFixed(2)}</div>}
+                   </CardContent>
+               </Card>
            </div>
+           <Table>
+               <TableCaption>A list of products</TableCaption>
+               <TableHeader>
+                   <TableRow>
+                       {(Array.isArray(file[0])) && file[0].map((el, index) => (
+                           <TableHead className="w-[100px]" key={index}>{el}</TableHead>
+                       ))}
+                   </TableRow>
+               </TableHeader>
+               <TableBody>
+                   {Array.isArray(file) && file.slice(1,file.length).map((el, index) => (
+                        <TableRow key={index}>
+                           {el.map((e, index) => (
+                               <TableCell key={index}>{e}</TableCell>
+                            ))}
+                        </TableRow>
+                   ))}
+               </TableBody>
+           </Table>
        </section>
     )
 }
